593bdfc0ff5678d761072479b72bcd03028b98c8
Kevin Lamping <klamping@gmail.com>
2016-11-25 10:15:00 -0600

    add examples in readme and such

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # node-spectre
-Node Library for Spectre Visual Regression Testing tool
+
+*This library is experimental and the API is likely to change.*
+
+Node Library for [Spectre, a Visual Regression Testing tool](https://github.com/wearefriday/spectre).
+
+## Installation
+
+```
+npm install --save node-spectre
+```
+
+## Usage
+
+```js
+const Spectre = require('node-spectre');
+
+spectre = new Spectre('https://path.to.spectre.server');
+
+spectre
+      .startRun('Project Name', 'Suite Name')
+      .then(function (id) {
+        spectre.uploadScreenshot(run_id, 'Test Name', 'Firefox', 'desktop', './screenshot.png');
+      })
+```
+
+[WebdriverIO](https://webdriver.io) example:
+
+```js
+const Spectre = require('node-spectre');
+
+describe('Login Page', () => {
+  let run_id, spectre;
+
+  before(function (done) {
+    spectre = new Spectre('https://node-spectre.herokuapp.com');
+
+    return spectre
+      .startRun('Test your login', this.test.parent.fullTitle())
+      .then(function (id) {
+        run_id = id;
+      })
+  })
+
+  afterEach(function () {
+    return spectre.uploadScreenshots()
+  });
+
+  it('should let you log in', function () {
+    browser.url('/');
+
+    let ss = browser.saveScreenshot();
+
+    spectre.queueScreenshot(run_id, 'login page', browser.desiredCapabilities.browserName, 'desktop', ss);
+
+    browser.setValue('[name="login"]', 'username');
+
+    ss = browser.saveScreenshot();
+
+    spectre.queueScreenshot(run_id, 'login page', browser.desiredCapabilities.browserName, 'desktop', ss);
+  })
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 const request = require('superagent');
-const fs = require('fs');
+const Promise = require('bluebird');
 
 class Spectre {
   constructor(url) {
     this.url = url;
+    this.screenshots = [];
   }
 
   startRun(project, suite) {
-    var res = request
-      .post(this.url + '/runs')
-      .send({ project, suite });
-
-    return res;
+    return new Promise((resolve, reject) => {
+      request
+        .post(this.url + '/runs')
+        .send({ project, suite })
+        .end((err, res) => {
+          err ? reject(err) : resolve(res.body.id);
+        });
+    });
   }
 
   uploadScreenshot(run_id, name, browser, width, screenshot) {
-    var res = request
-      .post(this.url + '/tests')
-      .field('test[run_id]', run_id)
-      .field('test[name]', name)
-      .field('test[browser]', browser)
-      .field('test[size]', width)
-      .field('test[highlight_colour]', "ff0000")
-      .attach('test[screenshot]', screenshot);
+    return new Promise((resolve, reject) => {
+      request
+        .post(this.url + '/tests')
+        .field('test[run_id]', run_id)
+        .field('test[name]', name)
+        .field('test[browser]', browser)
+        .field('test[size]', width)
+        .field('test[highlight_colour]', "ff0000")
+        .attach('test[screenshot]', screenshot)
+        .end((err, res) => {
+          err ? reject(err) : resolve(res);
+        });
+    });
+  }
 
-    return res;
+  uploadScreenshots() {
+    let uploads = [];
+
+    this.screenshots.forEach((screenshot) => {
+      uploads.push(screenshot());
+    });
+
+    return Promise.all(uploads);
+  }
+
+  queueScreenshot(run_id, name, browser, width, screenshot) {
+    this.screenshots.push(this.uploadScreenshot.bind(this, ...arguments));
   }
 }
 
